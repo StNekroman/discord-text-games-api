c450d89db7dfbe17a1bca579da747dbf3e2582f6
StNekroman <stnekroman@gmail.com>
2022-08-27 11:13:00 +0300

    build

--- dist/events/ButtonClickGameEvent.js
+++ dist/events/ButtonClickGameEvent.js
@@ -1,2 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ButtonClickGameEvent = void 0;
+const EventType_1 = require("./EventType");
+var ButtonClickGameEvent;
+(function (ButtonClickGameEvent) {
+    function isButtonClickGameEvent(event) {
+        return event.type === EventType_1.EventType.BUTTON_CLICK;
+    }
+    ButtonClickGameEvent.isButtonClickGameEvent = isButtonClickGameEvent;
+})(ButtonClickGameEvent = exports.ButtonClickGameEvent || (exports.ButtonClickGameEvent = {}));
--- dist/events/index.js
+++ dist/events/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserGameEvent = exports.UserEventTypes = exports.EventType = void 0;
+exports.UserGameEvent = exports.ButtonClickGameEvent = exports.UserEventTypes = exports.EventType = void 0;
 var EventType_1 = require("./EventType");
 Object.defineProperty(exports, "EventType", { enumerable: true, get: function () { return EventType_1.EventType; } });
 Object.defineProperty(exports, "UserEventTypes", { enumerable: true, get: function () { return EventType_1.UserEventTypes; } });
+var ButtonClickGameEvent_1 = require("./ButtonClickGameEvent");
+Object.defineProperty(exports, "ButtonClickGameEvent", { enumerable: true, get: function () { return ButtonClickGameEvent_1.ButtonClickGameEvent; } });
 var UserGameEvent_1 = require("./UserGameEvent");
 Object.defineProperty(exports, "UserGameEvent", { enumerable: true, get: function () { return UserGameEvent_1.UserGameEvent; } });
